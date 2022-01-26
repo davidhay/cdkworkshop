7525f926aa086814a3e773d608f8fce813e56634
David Hay <david.j.hay@gmail.com>
2022-01-26 15:37:11 +0000

    Add deploy stage to pipeline

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,4 +1,3 @@
-import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
@@ -6,7 +5,6 @@ import { Construct } from "constructs";
 export interface HitCounterProps {
   /** the function for which we want to count url hits **/
   downstream: lambda.IFunction;
-
   /**
    * The read capacity units for the table
    *
@@ -25,23 +23,12 @@ export class HitCounter extends Construct {
   public readonly table: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props: HitCounterProps) {
-    if (
-      props.readCapacity !== undefined &&
-      (props.readCapacity < 5 || props.readCapacity > 20)
-    ) {
-      throw new Error("readCapacity must be greater than 5 and less than 20");
-    }
     super(scope, id);
 
     const table = new dynamodb.Table(this, "Hits", {
-      partitionKey: {
-        name: "path",
-        type: dynamodb.AttributeType.STRING,
-      },
-      encryption: dynamodb.TableEncryption.AWS_MANAGED,
+      partitionKey: { name: "path", type: dynamodb.AttributeType.STRING },
       readCapacity: props.readCapacity ?? 5,
     });
-
     this.table = table;
 
     this.handler = new lambda.Function(this, "HitCounterHandler", {
