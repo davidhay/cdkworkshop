4ed655aacf11c012e8ceb4a87427166abde4440f
David Hay <david.j.hay@gmail.com>
2022-01-26 14:37:36 +0000

    tweak

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from "aws-cdk-lib";
 import * as codecommit from "aws-cdk-lib/aws-codecommit";
 import { Construct } from "constructs";
 
+//this is a tweak
+
 export class WorkshopPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
